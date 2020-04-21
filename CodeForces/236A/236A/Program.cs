5d8dee0e86d30e1cd2131871d27a35fbef1e4a2f
maciekwin3 <maciekwin3@gmail.com>
2020-04-22 00:09:46 +0200

    update

---
+++ CodeForces/236A/236A/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _236A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+                      
+            string chat = "CHAT WITH HER!";
+            string ignore = "IGNORE HIM!";
+            string message = Console.ReadLine();
+            char[] tab = message.ToCharArray();
+            Array.Sort(tab);
+            //Console.WriteLine(tab);
+            for(int i = 1; i < tab.Length; i++)
+            {
+                if(tab[i-1] != tab[i])
+                {
+                    counter++;
+                }
+            }
+            
+            
+            if(counter % 2 == 0)
+            {
+                Console.WriteLine(chat);
+            }
+            else
+            {
+                Console.WriteLine(ignore);
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
