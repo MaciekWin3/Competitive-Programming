2bd545f26dafa89c2cccdd1e0e751119017b624d
MaciekWin3 <maciekwin3@gmail.com>
2020-04-24 21:41:57 +0200

    Another 2 solutions

---
+++ CodeForces/703A/703A/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _703A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rounds = int.Parse(Console.ReadLine());
+            int i = 0;
+            char[] tab = new char[rounds * 2];
+            string tmp;
+            while(i < rounds * 2)
+            {
+                tmp = Console.ReadLine();
+                tab[i] = tmp[0];
+                tab[i + 1] = tmp[2];
+                i = i + 2;
+            }
+            int[] throws = new int[tab.Length];
+            i = 0;
+            int mishka = 0;
+            int chris = 0;
+            while(i < tab.Length)
+            {
+                throws[i] = Convert.ToInt32(tab[i]);
+                i++;
+            }           
+            i = 0;
+            while(i < throws.Length)
+            {
+                if(throws[i] > throws[i+1])
+                {
+                    mishka++;
+                }
+                else if (throws[i] < throws[i+1])
+                {
+                    chris++;
+                }
+                else
+                {
+                    mishka++;
+                    chris++;
+                }
+                i = i + 2;
+            }
+            if(mishka > chris)
+            {
+                Console.WriteLine("Mishka");
+            }
+            else if(mishka < chris)
+            {
+                Console.WriteLine("Chris");
+            }
+            else
+            {
+                Console.WriteLine("Friendship is magic!^^");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CodeForces/996A/996A/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _996A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int output = 0;
+            int one, five, ten, twenty, hundred;
+            one = five = ten = twenty = hundred = 0;
+            int counter = 0;
+            if(input >= 100)
+            {
+                hundred = input / 100;
+                input = input - (hundred * 100);
+            }
+            if (input >= 20)
+            {
+                twenty = input / 20;
+                input = input - (twenty * 20);
+            }
+            if (input >= 10)
+            {
+                ten = input / 10;
+                input = input - (ten * 10);
+            }
+            if (input >= 5)
+            {
+                five = input / 5;
+                input = input - (five * 5);
+            }
+            if (input >= 1)
+            {
+                one = input / 1;
+                input = input - (one * 1);
+            }
+            counter = hundred + twenty + five + ten + one;
+            Console.WriteLine(counter);
+            //Console.ReadKey();
+            
+        }
+    }
+}
