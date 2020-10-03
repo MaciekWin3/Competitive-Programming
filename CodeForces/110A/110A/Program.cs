f5e2458a7b1bd71ee27b87d06cdfd07a5e8bb299
maciekwin3 <maciekwin3@gmail.com>
2020-10-03 10:46:18 +0200

    Warm up

---
+++ CodeForces/110A/110A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _110A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int counter = 0;
+
+            for(int i=0; i<input.Length; i++)
+            {
+                if(input[i] == '4' || input[i] == '7')
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 4 || counter == 7)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
