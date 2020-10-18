f01298bf3e8de81943c6954f821399945f88bf86
maciekwin3 <maciekwin3@gmail.com>
2020-10-19 00:08:57 +0200

    59a

---
+++ CodeForces/59A/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _59A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counterUpper = 0;
+            int counterLower = 0;
+
+            string input = Console.ReadLine();
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsUpper(input[i]) == true)
+                {
+                    counterUpper++;
+                }
+                else
+                {
+                    counterLower++;
+                }
+            }
+            if(counterUpper > counterLower)
+            {
+                Console.WriteLine(input.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(input.ToLower());
+            }
+        }
+    }
+}
