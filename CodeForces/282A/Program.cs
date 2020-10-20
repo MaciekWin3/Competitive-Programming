5e19ebfaf8aa4b16d878b8344d3148f27a599171
maciekwin3 <maciekwin3@gmail.com>
2020-10-20 12:46:49 +0200

    282a

---
+++ CodeForces/282A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _282A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int output = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                string operation = Console.ReadLine();
+                if(operation.Contains('+'))
+                {
+                    output++;
+                }
+                else
+                {
+                    output--;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
