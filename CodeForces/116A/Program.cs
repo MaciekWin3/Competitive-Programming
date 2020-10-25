a58ad2bf2eb64c745072eed424c29b5e3b6c3176
maciekwin3 <maciekwin3@gmail.com>
2020-10-25 19:15:25 +0100

    commit

---
+++ CodeForces/116A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _116A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stops = int.Parse(Console.ReadLine());
+            int capacity = 0;
+            int max = 0;
+
+            for(int i = 0; i < stops; i++)
+            {
+                string [] passangers = Console.ReadLine().Split(" ");
+                int exit = int.Parse(passangers[0]); 
+                int enter = int.Parse(passangers[1]); 
+                if(i == 0)
+                {
+                    capacity = exit;
+                }
+                capacity = capacity - exit + enter;
+                if(capacity > max)
+                {
+                    max = capacity;
+                }
+
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
